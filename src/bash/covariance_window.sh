cb4133cef7698da82dcadb58e29b7591ae85d0be
jamieAmacpherson <macpherson.js@gmail.com>
2017-07-06 13:18:02 +0100

    dcd file addition

--- src/bash/covariance_window.sh
+++ src/bash/covariance_window.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-let b=k-$3
-
-echo $b
-echo $k
-
 if [ $# -ne 6 ]
 then
         echo "Incorrect number of arguments..."
@@ -12,12 +7,17 @@ then
         exit 1
 fi
 
-mdconvert='/home/macphej/jm.software/development/allosteric_signal/replica_ana/src/python/mdconvert.py'
-covarmat='/home/macphej/jm.software/development/allosteric_signal/replica_ana/src/python/covar_mat.py'
-overlap='/home/macphej/jm.software/development/allosteric_signal/replica_ana/src/python/MI_space.py'
+mdconvert='/home/macphej/jm.software/development/allosteric_signal/src/python/mdconvert.py'
+covarmat='/home/macphej/jm.software/development/allosteric_signal/src/python/covar_mat.py'
+overlap='/home/macphej/jm.software/development/allosteric_signal/src/python/MI_space.py'
 
-
-
+gmx trjconv -f $1\
+             -s $2 -n $6\
+             -b 0 -e 0\
+	     -o topol.pdb <<EOF
+3
+3
+EOF
 
 ## Split trajectory into blocks
 let k=$3
@@ -26,31 +26,25 @@ let nst=$4
 while [ $k -le $nst ]
 do
 
-
-
-echo $b
-echo $k
 gmx trjconv -f $1\
              -s $2 -n $6\
-             -b $b -e $k\
-	     -o $b.xtc <<EOF
+             -b 0 -e $k\
+	     -o $k.xtc <<EOF
 3
 3
 EOF
 
 
-python $mdconvert $b.xtc -o $b.dcd
+python $mdconvert $k.xtc -o $k.dcd
 
-rm $b.xtc 
 
-python $covarmat -t $b.dcd -s topol.pdb -b $b 
+python $covarmat -t $k.dcd -s topol.pdb -b $k 
 
+rm *.dcd *.xtc
 
 let k=k+$3
 done
 
-rm *.dcd
 
-python $overlap $5 --matrix yes
+python $overlap $5 --CO yes
 exit
-
