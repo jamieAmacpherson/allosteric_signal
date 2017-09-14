c67e9608125365081900a8c0bb365830cbc28e10
jkleinj <jens@jkleinj.eu>
2017-09-14 18:17:07 +0100

    fit and tau histogram

--- src/R/GPUtest.R
+++ src/R/GPUtest.R
@@ -1,19 +1,21 @@
-library(gputools)
+
+library(gputools);
 
 #_______________________________________________________________________________
-magnitude <- 10
-dimA <- 2*magnitude;dimB <- 3*magnitude;dimC <- 4*magnitude
-matA <- matrix(runif(dimA*dimB), dimA, dimB)
-matB <- matrix(runif(dimB*dimC), dimB, dimC)
+magnitude <- 10;
+dimA <- 2*magnitude;dimB <- 3*magnitude;dimC <- 4*magnitude;
+matA <- matrix(runif(dimA*dimB), dimA, dimB);
+matB <- matrix(runif(dimB*dimC), dimB, dimC);
 
 system.time(matA%*%matB);
-system.time(gpuMatMult(matA, matB))
+system.time(gpuMatMult(matA, matB));
 
 #_______________________________________________________________________________
-magnitude <- 1000
-dimA <- 2*magnitude;dimB <- 3*magnitude;dimC <- 4*magnitude
-matA <- matrix(runif(dimA*dimB), dimA, dimB)
-matB <- matrix(runif(dimB*dimC), dimB, dimC)
+magnitude <- 1000;
+dimA <- 2*magnitude;dimB <- 3*magnitude;dimC <- 4*magnitude;
+matA <- matrix(runif(dimA*dimB), dimA, dimB);
+matB <- matrix(runif(dimB*dimC), dimB, dimC);
 
 system.time(matA%*%matB);
-system.time(gpuMatMult(matA, matB))
+system.time(gpuMatMult(matA, matB));
+
--- src/R/autocorr/MI_noise_parallel.R
+++ src/R/autocorr/MI_noise_parallel.R
@@ -18,12 +18,14 @@ nCore <- detectCores() - 1;
 #______________________________________________________________________________
 ## MI matrices
 inPath = c("./pkm2");
-filenames = list.files(path = inPath, full.names = TRUE, pattern = 'lf_nMImat.out$');
+filenames = list.files(path = inPath, full.names = TRUE,
+	pattern = '^[0-9][0-9][0-9][0-9][0-9].lf_nMImat.out$');
 filenames.sort = mixedsort(filenames);
 
 tmp.df = read.table(filenames.sort[1]);
 MI = rep(list(matrix(NA, nrow = dim(tmp.df)[1], ncol = dim(tmp.df)[2]),
 			length(filenames.sort)));
+#for (i in 1:length(filenames.sort)) {
 for (i in 1:length(filenames.sort)) {
 	print(paste(i, filenames.sort[[i]]));
 	MI[[i]] = as.matrix(read.table(filenames.sort[i]));
@@ -34,7 +36,7 @@ for (i in 1:length(filenames.sort)) {
 #______________________________________________________________________________
 ## autocorrelation and exponential fit
 ## MI.v: position-specific Mutual Information vector; i: iteration index
-acMI = function(MI.v, i) {
+acMI = function(MI.v) {
 	## autocorrelation
 	MI_acf = acf(MI.v, plot = FALSE);
 
@@ -49,19 +51,23 @@ acMI = function(MI.v, i) {
 	## if fit successful, class of 'dat.nls' will be 'nls',
 	##	otherwise it will be 'try-error'
 	if (class(MI.nls) == c("nls")) {
-		fitval[[i]] = c(coef(MI.nls), deviance(MI.nls));
+		c(coef(MI.nls), deviance(MI.nls));
 	} else {
-		fitval[[i]] = c(0, 0, 0);
+		c(0, 0, 0);
 	}
 }
 
+#______________________________________________________________________________
 ## upper triange lindices
-rc = combn(1:dim(tmp.df)[1], 2);
+#rc = combn(1:dim(tmp.df)[1], 2);
+## up to 500 works fine
+rc = combn(1:500, 2);
 ## iteration indices
 rc = rbind(rc, 1:dim(rc)[2]);
 nRc = dim(rc)[2];
 
 ## data structure holding MI vectors of all positions
+## that is following the MI signal of each position over time
 MI.vl = rep(list(vector(mode = "numeric", length = length(MI))), nRc);
 for(i in 1:nRc) {
 	MI.vl[[i]] = rapply(MI, function(x) x[rc[1,i], rc[2,i]]);
@@ -69,23 +75,29 @@ for(i in 1:nRc) {
 
 ## data structure for fit values
 fitval = rep(list(vector(mode = "numeric", length = 3)), nRc);
-nIter = 0;
 
+#______________________________________________________________________________
 ## initiate cluster for parallel computation 
 clu = makeCluster(nCore);
 ## make parallel functions see predefined variables
-clusterExport(clu, c("MI.vl", "nIter", "acMI", "fitval"));
+clusterExport(clu, c("MI.vl", "acMI", "fitval"));
 
-invisible(parLapply(clu, MI.vl, function(x) { nIter <<- nIter + 1; acMI(x, nIter); }));
-
-## run acMI function over all index values
-## 'invisible' suppresses any output of the function evaluation
-#invisible(sapply(1:nRc, function(x) { acMI(rc[1, x], rc[2, x], rc[3, x]); }));
-
+## perform all fits
+fitval = parLapply(clu, MI.vl, function(x) { acMI(x); });
+## save results
 saveRDS(fitval, "fitval.RDS");
+## release memory
+stopCluster(clu);
 
 #______________________________________________________________________________
-stopCluster(clu);
+## evaluate fit results
+## remove zero elements
+fitval.nozero = fitval[lapply(fitval, sum) > 0];
+## extract taus
+tau = rapply(fitval.nozero, function(x) x[1]);
+## tau distribution
+hist(tau, breaks = 200, xlim = c(0, 5));
+
 
 #===============================================================================
 
