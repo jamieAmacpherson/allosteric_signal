57d0b3c808274c172d32e110f144d27e292b47af
jamieAmacpherson <macpherson.js@gmail.com>
2017-10-03 12:09:06 +0100

    split chains

--- test_sys/pkm2/ES_structures/split_all_domains.R
+++ test_sys/pkm2/ES_structures/split_all_domains.R
@@ -19,27 +19,27 @@ for (i in 1:length(filename)) {
 
 	## we will use only chain A (the first of four protomers)
 	## N-terminal domain
-	domN = atom.select(pdb, chain = "A", resno = 13:47);
+	domN = atom.select(pdb, resno = 13:47);
 	pdbN = trim.pdb(pdb, domN);
-	nameN = paste(substring(filename[i], 1, 6), "domN", "pdb", sep = ".");
+	nameN = paste(filename[i], "domN", "pdb", sep = ".");
 	write.pdb(pdbN, file = paste(outDir, nameN, sep = "/"));
 
 	## A domain
-	domA = atom.select(pdb, chain = "A", resno = c(48:116, 221:388));
+	domA = atom.select(pdb, resno = c(48:116, 221:388));
 	pdbA = trim.pdb(pdb, domA);
-	nameA = paste(substring(filename[i], 1, 6), "domA", "pdb", sep = ".");
+	nameA = paste(filename[i], "domA", "pdb", sep = ".");
 	write.pdb(pdbA, file = paste(outDir, nameA, sep = "/"));
 
 	## B domain
-	domB = atom.select(pdb, chain = "A", resno = 117:220);
+	domB = atom.select(pdb, resno = 117:220);
 	pdbB = trim.pdb(pdb, domB);
-	nameB = paste(substring(filename[i], 1, 6), "domB", "pdb", sep = ".");
+	nameB = paste(filename[i], "domB", "pdb", sep = ".");
 	write.pdb(pdbB, file = paste(outDir, nameB, sep = "/"));
 
 	## C domain
-	domC = atom.select(pdb, chain = "A", resno = 389:530);
+	domC = atom.select(pdb, resno = 389:530);
 	pdbC = trim.pdb(pdb, domC);
-	nameC = paste(substring(filename[i], 1, 6), "domC", "pdb", sep = ".");
+	nameC = paste(filename[i], "domC", "pdb", sep = ".");
 	write.pdb(pdbC, file = paste(outDir, nameC, sep = "/"));
 }
 
