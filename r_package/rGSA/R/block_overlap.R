bf73c6f2aa36d0c27c5d04d4cab8c456c2b134ac
jamieAmacpherson <macpherson.js@gmail.com>
2018-03-29 20:08:38 +0100

    initial R package commit

---
+++ r_package/rGSA/R/block_overlap.R
@@ -0,0 +1,51 @@
+#' Compute the covariance overlap between each of the trajectory blocks from an input list
+#' of eigensystems.
+#'
+#'
+#' @param eigensys.list A list of eigen-systems determined from comp_eigensystem()
+#' @param eigfrom First eigenvector considered in computing the covariance overlap
+#' @param eigto Last eigenvector considered in computing the covariance overlap
+#' @return A matrix containing the covariance overlap between each of the trajectory blocks
+#' @export 
+
+
+
+block_overlap = function(eigensys.list, eigfrom, eigto){
+
+	# parse default values if user does not specify
+	eigfrom = as.numeric(ifelse(is.na(eigfrom), 1, eigfrom))
+	eigto = as.numeric(ifelse(is.na(eigto), 10, eigto))
+
+	# number of blocks in trajectory	
+	nBlock = length(eigensys.list)
+
+	# matrix to store overlaps between all block pairs
+	traj.overlap = matrix(0, nrow = nBlock, ncol = nBlock)
+	
+	# range of eigenvalues used in calculating the covariance overlap
+	eigenrange = eigfrom:eigto
+
+
+	print("COMPUTING COVARIANCE OVERLAP OF MUTUAL INFORMATION MATRICE")
+	
+	# measure time taken to execute 
+	system.time(
+
+		# compute the covariance overlap between each combination of 
+		# eigen-systems
+
+		for (i in 1:(nBlock-1)) {
+			for (j in (i+1):nBlock) {
+				traj.overlap[i, j] = covar_overlap(eigensys.list[[i]], eigrange, eigensys.list[[j]], eigrange)
+
+				# the overlap between matrix i and matrix j is identical to the overlap between matrix j and matrix i
+				traj.overlap[j, i] = traj.overlap[i, j];
+			}
+
+		}
+
+	)
+
+	# return matrix containing covarince overlap values
+	return(traj.overlap)
+}
---
+++ r_package/rGSA/R/matrix_smooth.R
@@ -0,0 +1,38 @@
+#' Smooth the matrix of covariance overlaps using a kernal smoothing algorithm.
+#' 
+#' This function takes an input matrix and performs a kernal smoothing. The input is a single
+#' matrix and returns a single matrix with the same dimensions as that of the input matrix.
+#'
+#' @param overlap.matrix Matrix of covariance overlaps, computed using block_overlap()
+#' @param eigfrom First eigenvector considered in computing the covariance overlap
+#' @param eigto Last eigenvector considered in computing the covariance overlap
+#' @return A matrix containing the covariance overlap between each of the trajectory blocks
+#' @export
+
+
+
+matrix_smooth = function(overlap.matrix){
+
+	# set diagonal to '0' to limit overlap range to observed values
+	diag(overlap.matrix) = 0
+
+	# vectorise matrix values
+	overlap.matrix.v = as.vector(overlap.matrix)
+
+	## matrix indices of vector values
+	nx = dim(overlap.matrix)[1]
+	ny = dim(overlap.matrix)[2]
+	x = rep(1:nx, ny)
+	y = rep(1:ny, each = nx)
+	grid.xy = as.data.frame(cbind(x,y))
+	
+	## smooth vector values given grid indices, resulting in a list
+	overlap.matrix.s = fields::smooth.2d(overlap.matrix.v,
+		ind = grid.xy,
+		nrow = nx,
+		ncol = ny,
+		theta = 2)
+
+	# return smoothed matrix
+	return(overlap.matrix.s)
+}
