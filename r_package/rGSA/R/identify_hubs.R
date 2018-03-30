d8b509e11abee33118d128ab9fbc746db5a2cb54
jamieAmacpherson <macpherson.js@gmail.com>
2018-03-30 18:37:16 +0100

    identify hubs changes

--- r_package/rGSA/R/identify_hubs.R
+++ r_package/rGSA/R/identify_hubs.R
@@ -29,6 +29,12 @@ identify_hubs = function(sector.list, n.hubs){
 		n.hubs = 10
 	}
 
+	if(n.hubs > ncol(sector.list[[1]])){
+		writelines('WARNING \n Number of hubs selected exceeds total number of protein fragments.
+			\n All of the protein fragments will be sorted according to their predicted
+			contribution to the allosteric process. \n This may take a long time for large protein systems.')
+	}
+
 
 	# if there are more than a single ergodic sector in the list, then average over the ergodic
 	# sector mutual information matrices
@@ -51,6 +57,11 @@ identify_hubs = function(sector.list, n.hubs){
 		# variance-weight the mutual information matrix
 		w.mat = mat * matvar.T
 
+		# remove the lower triangle and diagonal elements of the 
+		# mutual information matrix
+		w.mat[lower.tri(w.mat)] = 0
+		diag(w.mat) = 0
+
 		# sort the mutual information according to the strength of the mutual information
 		# couplings
 		sorted.mat = sort(w.mat,
@@ -63,8 +74,6 @@ identify_hubs = function(sector.list, n.hubs){
 		# also capture the top mutual information couplings
 		# initialise a data frame to capture the output
 		top.couplings = data.frame()
-		names(top.couplings) = c('i', 'j')
-
 
 		# loop through the desired number of top-ranked hubs and extract the coupled fragments which
 		# are associated with the strongest coupling score (given by the highest mutual information)
@@ -81,6 +90,9 @@ identify_hubs = function(sector.list, n.hubs){
 			top.hubs = append(top.hubs, tmp.hubs)
 
 		}
+
+		# assign names to top.couplings dataframe
+		names(top.couplings) = c('i', 'j')
 
 		# extract a unique list of the top n number of hub fragments
 		top.n.hubs = unique(top.hubs)[c(1:n.hubs)]
@@ -104,6 +116,12 @@ identify_hubs = function(sector.list, n.hubs){
 		mat = sector.list[[1]]
 
 
+		# remove the lower triangle and diagonal elements of the 
+		# mutual information matrix
+		mat[lower.tri(mat)] = 0
+		diag(mat) = 0
+
+
 		# sort the mutual information according to the strength of the mutual information
 		# couplings
 		sorted.mat = sort(mat,
@@ -116,12 +134,12 @@ identify_hubs = function(sector.list, n.hubs){
 		# also capture the top mutual information couplings
 		# initialise a data frame to capture the output
 		top.couplings = data.frame()
-		names(top.couplings) = c('i', 'j')
 
 
 		# loop through the desired number of top-ranked hubs and extract the coupled fragments which
 		# are associated with the strongest coupling score (given by the highest mutual information)
-		for(i in seq(from = 1, to = length(sorted.mat))){
+		for(i in seq(from = 1, to = n.hubs)){
+
 
 			# determine the i, j indices of the sorted mutual information matrix
 			tmp.hubs = which(mat == sorted.mat[i], arr.ind = TRUE)
@@ -135,6 +153,9 @@ identify_hubs = function(sector.list, n.hubs){
 
 		}
 
+		# assign names to top.couplings dataframe
+		names(top.couplings) = c('i', 'j')
+
 		# extract a unique list of the top n number of hub fragments
 		top.n.hubs = unique(top.hubs)[c(1:n.hubs)]
 
@@ -145,6 +166,7 @@ identify_hubs = function(sector.list, n.hubs){
 		listnames = c('top.n.hubs', 'top.couplings')
 
 		names(out.dat) = listnames
+
 	}
 
 	# return a two-element list containing the top n hubs and the top couplings
